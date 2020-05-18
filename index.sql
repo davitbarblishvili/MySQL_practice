d3b27705acca297f506e114f16c5b2917dffab02
Davit Barblishvili <db3230@columbia.edu>
2020-05-17 20:41:29 -0400

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,18 @@
+SELECT * FROM salaries
+where 
+	salary >= 89000;
+
+-- Create Index for salary column
+CREATE INDEX i_salary on salaries(salary);
+
+/*------------------------------------------*/
+
+SELECT
+	* 
+FROM
+	employees
+WHERE
+	first_name = 'Georgi'
+		AND last_name = 'Facello';
+        
+CREATE INDEX i_full ON employees(first_name, last_name);
